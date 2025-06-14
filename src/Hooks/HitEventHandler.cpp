109ee94decd4798c074fa3fb08621689e912fb1f
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2025-06-14 20:23:32 +0100

    Update HitEventHandler.cpp

--- src/Hooks/HitEventHandler.cpp
+++ src/Hooks/HitEventHandler.cpp
@@ -119,8 +119,8 @@ float HitEventHandler::RecalculateStagger(RE::Actor* target, RE::Actor* aggresso
 	if (hitData->flags.all(RE::HitData::Flag::kBlocked)) {
 		if (stagger > 0.00) {
 			auto block_score = static_cast<uint32_t>(1.0f - (target->AsActorValueOwner()->GetActorValue(RE::ActorValue::kBlock) / 100.0f));
-			if (block_score < 0.35) {
-				block_score = 0.35;
+			if (block_score < 0.2) {
+				block_score = 0.2;
 			}
 			stagger *= block_score;
 		}
